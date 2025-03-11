e96536229801b0f63effc2b7dacb16a7a539fe0f
rroshchin18 <40370407+rroshchin18@users.noreply.github.com>
2025-03-11 18:55:53 -0400

    personal detail changes

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,13 +2,13 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
+  name: "Russ Roshchin",
+  initials: "RR",
   url: "https://dillion.io",
-  location: "San Francisco, CA",
+  location: "Baltimore, MD",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Former teacher and coach turned instructional designer. I love all things learning and helping people realize and maximize their skills.",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.png",
