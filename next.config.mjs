47f382a757fb68dbca175d4dd885ab5c8264615a
Russell Roshchin <40370407+rroshchin18@users.noreply.github.com>
2025-05-09 12:38:22 -0400

    Fix Next.js configuration for WebContainer

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@next/swc-wasm-nodejs': false,
+      '@next/swc-linux-x64-gnu': false,
+      '@next/swc-linux-x64-musl': false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
